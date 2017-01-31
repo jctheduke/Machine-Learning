72b6b9c615decbd081d103d9ebd237d9368f9165
jctheduke <priyaranjan780@gmail.com>
2017-01-30 20:40:36 -0700

    Docs : Made changes to alpha values to improve the smart performance.

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -211,7 +211,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent,learning = True, alpha = 0.85,epsilon=1)
+    agent = env.create_agent(LearningAgent,learning = True, alpha = 0.5,epsilon=1)
     # agent = env.create_agent(LearningAgent, learning=True)
     
     ##############
